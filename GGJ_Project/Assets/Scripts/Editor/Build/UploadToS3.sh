b519ed03096d0b1326adce08a2a3a787822bddfc
Luis Van Slageren <luisvsm@gmail.com>
2021-01-27 12:13:55 +1100

    More changes to Discord download link

--- GGJ_Project/Assets/Scripts/Editor/Build/UploadToS3.sh
+++ GGJ_Project/Assets/Scripts/Editor/Build/UploadToS3.sh
@@ -9,19 +9,19 @@ URL="https://builds.connectedplay.io/ggj2021"
 S3Address="s3://builds.connectedplay.io/ggj2021"
 BuildInfoFileLocation="./Assets/Scripts/Editor/Build/lastbuild.txt"
 CloudFrontDistID="E28MCYN6VPSOX9"
+CloudFrontKeyPairID="K166I4EZSMCK92"
 
 ### [ Calculated Values ] ###
 OneDayInSeconds=$((24 * 60 * 60))
 ExpiryTime=$(($(date +%s) + $((LinkExpiryInDays * OneDayInSeconds ))))
 BuildDirectory=$(cat $BuildInfoFileLocation)
 Platform=$1
-ZipName=$ProjectName-$Platform-$(date +'%d-%m-%y_%H-%M-%S').zip
+GitVersionHash=$(git rev-parse --short HEAD)
+ZipName=$ProjectName-$Platform-$GitVersionHash-$(date +'%d_%m_%y').zip
 ZipPath="./builds/$ZipName"
 URL="$URL/$ZipName"
-PrivateKey=~/CloudFront/private_connectedplay_builds_key.pem
-zip -r -X "$ZipPath" "$BuildDirectory"
+PrivateKey=~/CloudFront/private_connectedplay_builds_key.ProjectName
 
-KeyPairID="K166I4EZSMCK92"
 Policy="{
     \"Statement\": [
         {
@@ -38,10 +38,11 @@ Policy="{
 Base64Policy=$(echo "$Policy" | tr -d "\n" | tr -d " \t\n\r" | openssl base64 | tr -- '+=/' '-_~' | tr -d "\n" | tr -d " \t\n\r")
 SignedPolicy=$(echo "$Policy" | tr -d "\n" | tr -d " \t\n\r" | openssl sha1 -sign $PrivateKey | openssl base64 | tr -- '+=/' '-_~'| tr -d "\n" | tr -d " \t\n\r") 
 
-SignedURL="$URL?Policy=$Base64Policy&Signature=$SignedPolicy&Key-Pair-Id=$KeyPairID"
+SignedURL="$URL?Policy=$Base64Policy&Signature=$SignedPolicy&Key-Pair-Id=$CloudFrontKeyPairID"
 
 echo "$SignedURL"
 
+zip -r -X "$ZipPath" "$BuildDirectory"
 FileSize=$(ls -lah $ZipPath | awk -F " " {'print $5'})
 
 aws s3 cp "$ZipPath" "$S3Address/$ZipName" --profile $AWSProfile
@@ -51,9 +52,8 @@ aws cloudfront create-invalidation \
     --paths "/$ZipName" --profile $AWSProfile
 
 DateNow=$(date '+%Y-%m-%d %H:%M:%S')
-GitVersionHash=$(git rev-parse --short HEAD)
 DiscordPostData="{\
-    \"content\": \"New [$Platform build ($FileSize)]($URL) #$GitVersionHash uploaded at $DateNow\"
+    \"content\": \"New [$Platform build #$GitVersionHash ($FileSize)]($URL) uploaded at $DateNow\"
 }"
 curl -i -H "Accept: application/json" -H "Content-Type:application/json" \
 -X POST --data "$DiscordPostData" "$DiscordWebhook"
