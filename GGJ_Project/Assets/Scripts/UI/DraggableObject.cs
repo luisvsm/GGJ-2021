662dbc05f0c6447283971334aa078069a67affb1
Luis Van Slageren <luisvsm@gmail.com>
2021-01-30 02:01:34 +1100

    Created a game UI scene, so that we can load that in at runtime. Added bare bones of a drag and drop system that works between UI and world positions 	1. click or touch the UI water button 	2. let go on the smily face 	3. Check logs to see that the game object name was displayed! Yay 	4. TODO: Move sprite over your mouse / finger position while you're dragging 	5. TODO: Call a function or something on the thing you stopped dragging on.

---
+++ GGJ_Project/Assets/Scripts/UI/DraggableObject.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DraggableObject : MonoBehaviour, IPointerDownHandler
+{
+    bool dragging = false;
+
+    void Update()
+    {
+        DetectStopDragging();
+    }
+
+    void DetectStopDragging()
+    {
+        // Touch phase ended or mouse up
+        if (
+            dragging &&
+            ((Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended) ||
+            Input.GetMouseButtonUp(0))
+        )
+        {
+            StopDragging();
+        }
+    }
+
+    void StartDragging()
+    {
+        Debug.Log("Start");
+        dragging = true;
+    }
+
+    void StopDragging()
+    {
+        Debug.Log("Stop");
+        dragging = false;
+        Raycast();
+    }
+
+    void Raycast()
+    {
+        Vector2 worldPoint;
+        if (Input.touchCount == 1)
+        {
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            worldPoint = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+        }
+        else
+        {
+            //Get the mouse position on the screen and send a raycast into the game world from that position.
+            worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+
+        RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+        //If something was hit, the RaycastHit2D.collider will not be null.
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.name);
+        }
+    }
+
+    // Fires when touch start or mouse down happens
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        // Don't want to deal with multi-touch >_< lol
+        if (Input.touchCount <= 1)
+        {
+            StartDragging();
+        }
+    }
+}
