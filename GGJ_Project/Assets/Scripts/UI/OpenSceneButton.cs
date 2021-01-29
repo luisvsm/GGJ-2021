816cb94ad14fd94540eab8cb9dd56601f6be169b
Anna Tito <atito@annatito.com>
2021-01-29 22:03:44 +1100

    Scene Menu and test Scenes

---
+++ GGJ_Project/Assets/Scripts/UI/OpenSceneButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenSceneButton : MonoBehaviour
+{
+    [Tooltip("The name of the scene file exactly as it is in editor and remember if it is a new scene to add it in the build settinsg")]
+    public string _sceneName = "GreenHouseGameScene";
+
+    public void TriggerOpenScene()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
