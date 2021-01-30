5709e75807432dc9889654f7d8668f8262c08452
Luis Van Slageren <luisvsm@gmail.com>
2021-01-31 03:24:06 +1100

    Working on audio Making water can not have cactus in it

--- GGJ_Project/Assets/Scripts/OpenOutsideDoor.cs
+++ GGJ_Project/Assets/Scripts/OpenOutsideDoor.cs
@@ -6,6 +6,8 @@ public class OpenOutsideDoor : MonoBehaviour
 {
     public void OpenDoor()
     {
+        Debug.Log("Playing Audio");
+        AudioController.Play("SFX_Door_Open");
         RoomController.Instance.showRoom(3);
     }
 }
