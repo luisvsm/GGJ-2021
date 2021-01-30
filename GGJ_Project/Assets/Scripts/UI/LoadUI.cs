1f6d97e3ee42548a01a594ab0c848c35e45d29c6
Luis Van Slageren <luisvsm@gmail.com>
2021-01-30 15:22:47 +1100

    Made right menu bar a little wider Set sorting order on draggable objects to 10 so that they draw over the top of everything else Removed doubled up scripts on Right and Bottom menu bar Added script to load the UI scene Added UI loader to greenhouse scene

---
+++ GGJ_Project/Assets/Scripts/UI/LoadUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadUI : MonoBehaviour
+{
+    private static bool hasLoadedUIScene = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!hasLoadedUIScene)
+        {
+            SceneManager.LoadScene("GameUI", LoadSceneMode.Additive);
+            hasLoadedUIScene = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
