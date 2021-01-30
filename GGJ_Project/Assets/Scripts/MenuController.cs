efbb3a260ad008ae159b9d9bb921aa1f9eeb0bfb
Luis Van Slageren <luisvsm@gmail.com>
2021-01-30 19:58:27 +1100

    Set up one menu per 'room'

---
+++ GGJ_Project/Assets/Scripts/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviourSingleton<MenuController>
+{
+    public List<GameObject> MenuList;
+    public int startingRoom = 0;
+
+    public static MenuController instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.Find("MenuController").GetComponent<MenuController>();
+            }
+            return _instance;
+        }
+    }
+    private static MenuController _instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+
+        showMenu(startingRoom);
+    }
+
+    public void showMenu(int menuIndex)
+    {
+        for (int i = 0; i < MenuList.Count; i++)
+        {
+            MenuList[i].SetActive(i == menuIndex);
+        }
+    }
+}
