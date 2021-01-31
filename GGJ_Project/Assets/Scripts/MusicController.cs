6883d138764b52189e5e33c1dc301d369dc4b4cc
Luis Van Slageren <luisvsm@gmail.com>
2021-01-31 13:44:41 +1100

    Added music controller

---
+++ GGJ_Project/Assets/Scripts/MusicController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ClockStone;
+public class MusicController : MonoBehaviourSingleton<MusicController>
+{
+    public List<string> plantSongs;
+    public List<string> plantName;
+    private Dictionary<string, AudioObject> plantSongObjects = new Dictionary<string, AudioObject>();
+
+    void Start()
+    {
+        AudioController.Play("MUS_GameLoop_BackingTrack_Percussion");
+
+        for (int i = 0; i < plantSongs.Count; i++)
+        {
+            AudioObject tempObj = AudioController.Play(plantSongs[i]);
+            if (tempObj == null)
+            {
+                Debug.Log("Unable to load " + plantSongs[i]);
+            }
+            else
+            {
+                plantSongObjects.Add(plantName[i], AudioController.Play(plantSongs[i]));
+            }
+            //plantSongObjects[plantName[i]].volume = 0f;
+        }
+    }
+
+    public void PlaySong(string plant)
+    {
+        plantSongObjects[plant].volume = 1f;
+    }
+
+    public void PauseSong(string plant)
+    {
+        plantSongObjects[plant].volume = 0f;
+    }
+}
