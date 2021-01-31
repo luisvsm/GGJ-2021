166b3c6207d29709e911bae3531cffdb190199dc
Luis Van Slageren <luisvsm@gmail.com>
2021-01-31 16:17:27 +1100

    Added tap to add resources

---
+++ GGJ_Project/Assets/Scripts/TapToAddResource.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapToAddResource : MonoBehaviour
+{
+    public int amountToAdd;
+    public GameDataMonoSingleton.RESOURCE_TYPE resourceType;
+    // Start is called before the first frame update
+
+    void OnMouseDown()
+    {
+        if (resourceType == GameDataMonoSingleton.RESOURCE_TYPE.water)
+        {
+            PlayerInventoryMonoSingleton.Instance.CollectWater(amountToAdd);
+            AudioController.Play("SFX_WaterCan_GetWater");
+        }
+        else if (resourceType == GameDataMonoSingleton.RESOURCE_TYPE.poo)
+        {
+            PlayerInventoryMonoSingleton.Instance.CollectPoo(amountToAdd);
+            AudioController.Play("SFX_Poo_GetPoo");
+        }
+    }
+}
