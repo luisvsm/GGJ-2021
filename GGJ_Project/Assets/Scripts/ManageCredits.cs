4c7c21939399715c1fdf2d24b4ffff65a88fe033
Luis Van Slageren <luisvsm@gmail.com>
2021-01-31 18:19:31 +1100

    Added credits prefab

---
+++ GGJ_Project/Assets/Scripts/ManageCredits.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageCredits : MonoBehaviourSingleton<ManageCredits>
+{
+    public bool shown = false;
+    float skipTime;
+    public float timeToWaitUntilPlayerCanSkip = 1.5f;
+
+    void OnEnable()
+    {
+        shown = true;
+        skipTime = Time.time + timeToWaitUntilPlayerCanSkip;
+    }
+
+    void OnMouseDown()
+    {
+        if (skipTime < Time.time)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
