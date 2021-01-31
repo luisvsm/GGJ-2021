b26ec4ca03224307b0e9639936af7a6fdf99d168
Luis Van Slageren <luisvsm@gmail.com>
2021-01-31 15:29:33 +1100

    Added intro text

---
+++ GGJ_Project/Assets/Scripts/JankyGameText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JankyGameText : MonoBehaviour
+{
+    public GameObject nextGameText;
+
+    public void OnMouseDown()
+    {
+        if (nextGameText != null)
+        {
+            nextGameText.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            RoomController.Instance.showRoom(3);
+        }
+    }
+}
